e982693cc591096db024952d696bbf066c09805f
Moshe Nahmias <moshegrey@gmail.com>
2025-02-07 23:46:13 +0200

    add str for weight

--- weight/models.py
+++ weight/models.py
@@ -21,5 +21,8 @@ class Weight(Base):
     old_weight = DecimalField(max_digits=4, decimal_places=1)
     new_weight = DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
 
+    def __str__(self):
+        return f'{str(self.date)}- {str(self.old_weight)} - {str(self.new_weight)}'
+
     def get_absolute_url(self):
         return reverse('weight_list')
